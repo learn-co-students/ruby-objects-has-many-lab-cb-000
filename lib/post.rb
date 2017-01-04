47a9fe45b09f374136cb8f5689bf9f4c93dd40fe
David Slivken <slivken@gmail.com>
2017-01-04 18:15:36 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,20 @@
+class Post
+  attr_accessor :title, :author
+
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    @author = nil
+    @@all << self
+  end
+
+  def author_name
+    if @author != nil
+      @author.name
+    else
+      nil
+    end
+  end
+
+end
