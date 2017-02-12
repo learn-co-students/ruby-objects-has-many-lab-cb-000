7454aace2cf3512018c1869824d2316483112c72
Andrew Schubert <andrew.schubert1986@gmail.com>
2017-02-12 21:23:40 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,31 @@
 class Artist
   attr_accessor :name, :song
+#/// Class Methods ///
+  @@song_count = 0 #=> Class Counter
 
-  @@song_count = 0
-
-  def initialize(name)
-    @name = name
-    @songs = []
+  def self.song_count #=> Class method, returns Class Counter
+    @@song_count
   end
 
-  def songs
-    @songs
+#/// Instance Methods ///
+  def initialize(name) #=> Initializes a new Artist with name.
+    @name = name
+    @songs = [] #=> Keeps track of all songs by this artist.
   end
 
   def add_song(song)
-    self.songs << song
-    song.artist = self
-    @@song_count += 1
+    self.songs << song #=> Adds new song to that artist's song list.
+    song.artist = self #=> Associates this artist w/ the instance from the Song class.
+    @@song_count += 1 #=> Keeps track of total number of songs created from ALL artists.
   end
 
   def add_song_by_name(name)
-    name = Song.new(name)
-    add_song(name)
+    name = Song.new(name) #=> creates an instance of a new song from Song class.
+    add_song(name) #=> This calls on the #add_song from above....
   end
 
-  def self.song_count
-    @@song_count
+  def songs #=> returns the total number of songs by a particular artists.
+    @songs
   end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,11 @@
 class Song
   attr_accessor :name, :artist
 
-  def initialize(name)
+  def initialize(name) #=> Creates a new instance of a song with a name.
     @name = name
   end
 
-  def artist_name
+  def artist_name #=> Returns the artist's name of the song || Nil if the song has no artist.
     @artist != nil ? self.artist.name : nil
   end
 
