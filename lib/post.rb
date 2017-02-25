3cd06b6f26e7c26eea378ad8b5e74f9c21ee70ad
Tyler Bristol <tahbristol@gmail.com>
2017-02-25 02:03:47 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,17 @@
+class Post
+  attr_accessor :author, :title
+
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def author_name
+    @author != nil ? self.author.name : nil
+  end
+
+
+end
