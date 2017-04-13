840d6395a18451d591e6956226517e387e378325
Gabriel Constantin Staicu <gabriel_staicu@yahoo.com>
2017-04-13 13:18:51 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,40 @@
+#require 'pry'
+#require_relative './song.rb'
+
+class Artist
+  @@song_count = 0
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def self.song_count
+    @@song_count
+  end
+
+end
+
+#adele = Artist.new("Adele")
+#adele.add_song_by_name("Rolling in the Deep")
+#adele.songs.last.artist
+#
+#puts adele.songs.last.name
+#
+#binding.pry
+#
+#puts "here ends"
