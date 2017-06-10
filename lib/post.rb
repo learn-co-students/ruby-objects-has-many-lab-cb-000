b77c054ccfb5ac71a9c4fd5be339b13c278a0b54
cptbrkfst <dyl@resonategroup.ca>
2017-06-10 13:12:53 -0400

    Completed lab

---
+++ lib/post.rb
@@ -0,0 +1,13 @@
+require 'pry'
+# Post Class
+class Post
+  attr_accessor :title, :author
+
+  def initialize(title)
+    @title = title
+  end
+
+  def author_name
+    author ? author.name : nil
+  end
+end
