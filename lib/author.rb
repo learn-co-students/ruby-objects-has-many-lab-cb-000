ac9d4951df4177d69aad624a2e3e1258e1140c9b
Patrick Townley <patricktownley@gmail.com>
2017-08-02 03:37:52 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,26 @@
+class Author
+  # Count of post for *all* authors
+  @@post_count = 0
+  attr_accessor :name, :posts
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    post.author = self
+    @posts << post
+    @@post_count += 1
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
+  end
+
+  def self.post_count
+    @@post_count
+  end
+
+end
