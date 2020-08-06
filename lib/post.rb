6bb1721bef0ec7bc75589a0532afb81933e2d8c1
Steve Cordova <cordovas@bc.edu>
2020-08-06 04:31:46 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,27 @@
+class Post
+
+  attr_accessor :author, :title
+
+@@all = []
+
+def initialize(title)
+  @title = title
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+# def title
+# end
+
+# def author
+#   self.author = @title
+# end
+
+def author_name
+  author.name if author
+end
+
+end
